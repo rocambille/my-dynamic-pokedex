d7de0f26e224e1ab6f52a2caf86c20e917822448
romaing <rocambille@gmail.com>
2023-04-12 10:36:03 +0200

    to serve and to fetch

--- backend/src/router.js
+++ backend/src/router.js
@@ -4,6 +4,16 @@ const router = express.Router();
 
 const itemControllers = require("./controllers/itemControllers");
 
+const pokemonList = [
+  { id: 1, name: "bulbasaur" },
+  { id: 2, name: "charmander" },
+  { id: 3, name: "squirtle" },
+];
+
+router.get("/pokemons", (req, res) => {
+  res.json(pokemonList);
+});
+
 router.get("/items", itemControllers.browse);
 router.get("/items/:id", itemControllers.read);
 router.put("/items/:id", itemControllers.edit);
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,30 @@
+import { useState } from "react";
+
 import Home from "./pages/Home";
 
 import "./App.css";
 
 function App() {
+  const [fetchedData, setFetchedData] = useState([]);
+
+  const fetchOneTime = () => {
+    fetch("http://localhost:5000/pokemons")
+      .then((response) => response.json())
+      .then((data) => {
+        setFetchedData(data);
+      });
+  };
+
   return (
     <div className="App">
       <Home />
       <p>coucou</p>
+      <button type="button" onClick={fetchOneTime}>
+        click me
+      </button>
+      {fetchedData.map((pokemon) => (
+        <p key={pokemon.id}>{pokemon.name}</p>
+      ))}
     </div>
   );
 }
